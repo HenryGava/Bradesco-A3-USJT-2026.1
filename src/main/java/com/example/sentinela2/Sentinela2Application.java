69ba5a1963869a9f8d92d01c31a1704261f82a87
Jhonattan Almeida <jhonattan.primoge@hotmail.com>
2026-05-17 20:34:44 -0300

    Criação da estrutura MVC e API de denúncias

---
+++ src/main/java/com/example/sentinela2/Sentinela2Application.java
@@ -0,0 +1,13 @@
+package com.example.sentinela2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sentinela2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sentinela2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sentinela2/Sentinela2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sentinela2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sentinela2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
