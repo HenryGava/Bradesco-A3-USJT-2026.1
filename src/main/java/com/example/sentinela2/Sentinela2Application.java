a9f37c8c4e221808a3d9de0ba3bd1a4c9bea8722
phcarvalho19 <phcarvalhov16@gmail.com>
2026-05-18 16:59:58 -0300

    estrutura mvc back end montada

--- src/main/java/com/example/sentinela2/Sentinela2Application.java
+++ src/main/java/com/example/sentinela2/Sentinela2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Sentinela2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Sentinela2Application.class, args);
     }
-
 }
